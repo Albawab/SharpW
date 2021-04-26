c3e59297fd67674f7d087eeb4cae3a1496031c28
albawab <albawab94@gmail.com>
2021-04-26 14:03:18 +0200

    Add Grid EditTemplate

---
+++ 00 Blazor/Syncfusion/27 Grid EditTemplate DateTime Validatie/27 Grid EditTemplate DateTime Validatie/Data/WeatherForecast.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace _27_Grid_EditTemplate_DateTime_Validatie.Data
+{
+    public class WeatherForecast
+    {
+        public DateTime? Date { get; set; }
+
+        public int TemperatureC { get; set; }
+    }
+}
