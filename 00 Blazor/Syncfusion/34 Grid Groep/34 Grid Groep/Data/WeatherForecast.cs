5f40fe31bd30b642ee06015e308edb759592a7ee
albawab <albawab94@gmail.com>
2021-09-28 15:49:16 +0200

    Add tree grid syncfusion

---
+++ 00 Blazor/Syncfusion/34 Grid Groep/34 Grid Groep/Data/WeatherForecast.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace _34_Grid_Groep.Data
+{
+    public class WeatherForecast
+    {
+        public DateTime Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public string Summary { get; set; }
+    }
+}
