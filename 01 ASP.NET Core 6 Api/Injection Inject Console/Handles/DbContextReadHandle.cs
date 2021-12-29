adb09c53a224dce45554dfc68949d68b4a3000c9
albawab <albawab94@gmail.com>
2021-12-29 11:37:49 +0100

    Add Inject

---
+++ 01 ASP.NET Core 6 Api/Injection Inject Console/Handles/DbContextReadHandle.cs
@@ -0,0 +1,62 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using Stichting.Doneren.DbContextRead;
+using Stichting.Doneren.DomeinRead.Models.Users;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stichting.Doneren.DbContext.Read.Handler.Handles
+{
+    public class DbContextReadHandle
+    {
+        private readonly StichtingDbContextRead stichtingDbContextRead;
+
+        public DbContextReadHandle(StichtingDbContextRead stichtingDbContextRead)
+        {
+            this.stichtingDbContextRead = stichtingDbContextRead;
+        }
+
+        public void Handle()
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            factory.UserName = "test";
+            factory.Password = "test";
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.QueueDeclare(queue: "registerUser",
+                                     durable: false,
+                                     exclusive: false,
+                                     autoDelete: false,
+                                     arguments: null);
+
+                var consumer = new EventingBasicConsumer(channel);
+                consumer.Received += (model, ea) =>
+                {
+                    var body = ea.Body.ToArray();
+                    var message = Encoding.UTF8.GetString(body);
+
+                    var user = System.Text.Json.JsonSerializer.Deserialize<User>(message);
+                    if (user != null)
+                    {
+                        Console.WriteLine($" [x] Received {user.Name} User", message);
+                        // Toevoegen de gebruiker aan de db.
+                        stichtingDbContextRead.Users.Add(user);
+
+                        // Opslaan de gewijzigde in de db context.
+                        stichtingDbContextRead.SaveChangesAsync();
+                    }
+                };
+                channel.BasicConsume(queue: "registerUser",
+                                      autoAck: true,
+                                     consumer: consumer);
+
+
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ 01 ASP.NET Core 6 Api/Injection Inject Console/Program.cs
@@ -0,0 +1,45 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using Stichting.Doneren.DbContext.Read.Handler.Handles;
+using Stichting.Doneren.DomeinRead.Models.Users;
+using System.Text;
+
+
+
+
+var services = new ServiceCollection();
+ConfigureServices(services);
+services
+    .AddSingleton<DbContextReadHandle, DbContextReadHandle>()
+    .BuildServiceProvider()
+    .GetService<DbContextReadHandle>()
+    .Handle();
+
+// ================================>==================================> هون حطينا السيرفس حتا نقدر نعمل inject
+
+static void ConfigureServices(IServiceCollection services)
+{
+    services.AddDbContext<Stichting.Doneren.DbContextRead.StichtingDbContextRead>(options =>
+    {
+        options.UseSqlServer(
+            "Server=SQLDEVELOP\\SQL2017;Initial Catalog=Stichting.DonerenRead;User ID=sa;Password=Sebas0412;MultipleActiveResultSets=true",
+            b => b.MigrationsAssembly(typeof(Stichting.Doneren.DbContextRead.StichtingDbContextRead).Assembly.FullName));
+    });
+}
+
+
+/*using IHost host = Host.CreateDefaultBuilder(args)
+    .ConfigureServices((_, services) =>
+                services.AddDbContext<Stichting.Doneren.DbContextRead.StichtingDbContextRead>(options =>
+                {
+                    options.UseSqlServer(
+                        "Server=SQLDEVELOP\\SQL2017;Initial Catalog=Stichting.DonerenRead;User ID=sa;Password=Sebas0412;MultipleActiveResultSets=true",
+                        b => b.MigrationsAssembly(typeof(Stichting.Doneren.DbContextRead.StichtingDbContextRead).Assembly.FullName));
+                })
+                .AddSingleton<IHandleDbContextRead, DbContextReadHandler>()).Build().Services.GetService<DbContextReadHandle>().Handle();
+
+await host.RunAsync();*/
